8c37a61745e91352e682fbca8cbf46278b0989e1
Denay2468 <denayaung13@gmail.com>
2026-03-31 11:26:12 +0630

    Add inputData procedure

--- BMI/P4.cpp
+++ BMI/P4.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 
+void inputData(float &weight, float &height){
+    cout << "--- Welcome Student Class A 2025 ---" << endl;
+    cout << "Enter weight (kg): ";
+    cin >> weight;
+    cout << "Enter height (meter): ";
+    cin >> height;
+}
+
 int main(){
 
 }
